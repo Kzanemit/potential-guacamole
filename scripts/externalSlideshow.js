8fe0c556dda3afd6ce724dce12a80ddcd89991c6
Kzanemit <kyeannnazanemitchell@gmail.com>
2025-11-12 18:21:25 -0500

    Update externalSlideshow.js

--- scripts/externalSlideshow.js
+++ scripts/externalSlideshow.js
@@ -1,1 +1,19 @@
+/* External JavaScript: externalSlideshow.js
+   Source: adapted from DynamicDrive.com */
+const slides = [
+  "Welcome to my portfolio site!",
+  "Explore my creative projects.",
+  "JavaScript makes pages interactive!"
+];
+
+let slideIndex = 0;
+const slideshow = document.getElementById("slideshow");
+
+function showSlide() {
+  slideshow.innerHTML = `<h4>${slides[slideIndex]}</h4>`;
+  slideIndex = (slideIndex + 1) % slides.length;
+}
+
+setInterval(showSlide, 2000);
+showSlide();
 
